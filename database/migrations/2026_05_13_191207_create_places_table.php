5f486153f40a3572c08df06459c4eca00af615be
Caleb KPANOU <caleb.kpanou@chem.loc>
2026-05-21 12:09:24 +0200

    Pour presentation

--- database/migrations/2026_05_13_191207_create_places_table.php
+++ database/migrations/2026_05_13_191207_create_places_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::dropIfExists('places');
         Schema::create('places', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('city_id')->nullable()->constrained()->onDelete('set null');
+            $table->foreignId('city_id')->nullable()->constrained()->onDelete('sdet null');
             $table->string('nom');
             $table->string('ville')->nullable();
             $table->string('departement')->nullable();
